28819f5ed7333a679465756dbd042d6bbc29f3b5
breno.monte <breno.monte@unimedteresina.com.br>
2020-04-22 14:05:00 -0300

    Cleaning the Project

---
+++ lib/widgets/painter.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Painter extends CustomPainter {
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    //TODO: CREATE HOURGLASS
+  }
+
+  @override
+  bool shouldRepaint(Painter old) {
+    return null;
+  }
+}
